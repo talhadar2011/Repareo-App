ecc0b8c18607de412239cca68bab1c2853d6e98e
Talha <talhadar2011@gmail.com>
2025-09-25 23:09:32 +0200

    Final Adjustment to dynamic stepper complete

--- components/stepper/stepperbar.tsx
+++ components/stepper/stepperbar.tsx
@@ -12,7 +12,7 @@ export default function stepperbar({Step,index,st}:PropsType) {
 <>				<div className='flow flow-col'>
 					<div key={index}  className={`flex  items-center justify-center `}>
 
-					<div className={`${index<=Step?"bg-blue-500 text-white":"bg-gray-300"}  w-10 h-10 rounded-full flex items-center justify-center font-extrabold `}>
+					<div className={`${index<=Step?"bg-blue-500 text-white":"bg-gray-300"}  w-10 h-10 md:w-12 md:h-12 lg:w-14 lg:h-14 rounded-full flex items-center justify-center font-extrabold `}>
 					{index+1}
 					</div>
 					{(index+1===steps.length)?<div className='flex-1'></div>:<div className={`flex-1 h-1 top-15 w-10 md:w-24 lg:w-30  -z-10 ${index<Step?"bg-blue-500":"bg-gray-300"}`}></div>}
